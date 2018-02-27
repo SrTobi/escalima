7c2f01052c856df445592d8315f29f9440e6036a
SrTobi <code.databyte@gmail.com>
2018-02-27 23:17:03 +0100

    added docs for public methods/classes

--- escalima/js/src/main/scala/escalima/JSESPrimaBridge.scala
+++ escalima/js/src/main/scala/escalima/JSESPrimaBridge.scala
@@ -6,7 +6,12 @@ private class JSESPrimaBridge extends ESPrimaBridge {
     override def parse(source: String, module: Boolean): String = {
         val parse = if (module) ESPrima.parseModule _ else ESPrima.parseScript _
         val options = new ESPrimaOptions(true)
-        val astJson = parse(source, options)
-        JSON.stringify(astJson)
+        try {
+            val astJson = parse(source, options)
+            JSON.stringify(astJson)
+        } catch {
+            case e: Exception =>
+                throw ESPrimaParseException(e.getMessage)
+        }
     }
 }
--- escalima/shared/src/main/scala/escalima/ECMAScript.scala
+++ escalima/shared/src/main/scala/escalima/ECMAScript.scala
@@ -1,12 +1,61 @@
 package escalima
 
+/**
+  * The main class of Escalima.
+  *
+  * Has methods to parse JavaScript.
+  *
+  * @note Creation of this class might be expensive! It is recommended to only create one per thread.
+  */
 class ECMAScript {
     private val bridge: ESPrimaBridge = ESPrimaBridgeCreator.create()
 
+    /**
+      * Parses a JavaScript module and returns it's ast.
+      *
+      * @note JavaScript modules and JavaScript scripts are different things. Scripts for example may not contain import/export declarations.
+      *
+      * @param sourcecode the source code that should be parsed
+      * @return the ast of the module
+      *
+      * @throws ESPrimaParseException if esprima was not able to parse sourcecode
+      */
     def parseModule(sourcecode: String): ast.Program = convertToAst(parseModuleToJson(sourcecode))
+
+    /**
+      * Parses a JavaScript script and returns it's ast.
+      *
+      * @note JavaScript modules and JavaScript scripts are different things. Scripts for example may not contain import/export declarations.
+      *
+      * @param sourcecode the source code that should be parsed
+      * @return the ast of the module
+      *
+      * @throws ESPrimaParseException if esprima was not able to parse sourcecode
+      */
     def parseScript(sourcecode: String): ast.Program = convertToAst(parseScriptToJson(sourcecode))
 
+    /**
+      * Parses a JavaScript module and returns the json created by esprima.
+      *
+      * @note JavaScript modules and JavaScript scripts are different things. Scripts for example may not contain import/export declarations.
+      *
+      * @param sourcecode the source code that should be parsed
+      * @return the json of the ast created by esprima
+      *
+      * @throws ESPrimaParseException if esprima was not able to parse sourcecode
+      */
     def parseModuleToJson(sourcecode: String): String = bridge.parse(sourcecode, module = true)
+
+    /**
+      * Parses a JavaScript script and returns the json created by esprima.
+      *
+      * @note JavaScript modules and JavaScript scripts are different things. Scripts for example may not contain import/export declarations.
+      *
+      * @param sourcecode the source code that should be parsed
+      * @return the json of the ast created by esprima
+      *
+      * @throws ESPrimaParseException if esprima was not able to parse sourcecode
+      */
     def parseScriptToJson(sourcecode: String): String = bridge.parse(sourcecode, module = false)
 
     private def convertToAst(json: String): ast.Program = ast.Program.from(upickle.json.read(json))
